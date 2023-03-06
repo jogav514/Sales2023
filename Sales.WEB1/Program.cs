d79fe3274ed5b261a99567a6e57184a4e7a6849e
jogav514 <esteban1gaviria@gmail.com>
2023-03-06 13:58:08 -0500

    Chage Sales.Web failed server - ok, sweet alert

--- Sales.WEB1/Program.cs
+++ Sales.WEB1/Program.cs
@@ -1,7 +1,8 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sales.WEB;
 using Sales.WEB.Repositories;
+using Sales.WEB;
 using Web.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,7 +10,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7153") });
-builder.Services.AddScoped<IRepository,Repository>();
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 
 await builder.Build().RunAsync();
