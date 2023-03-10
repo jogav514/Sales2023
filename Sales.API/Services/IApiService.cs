020a2e266f440a50a79ae08f60b410173c35ac57
jogav514 <esteban1gaviria@gmail.com>
2023-03-10 14:47:06 -0500

    Database country full records

---
+++ Sales.API/Services/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using Sales.Shared.Responses;
+
+namespace Sales.API.Services
+{
+    public interface IApiService
+    {
+
+        
+        Task<Response> GetListAsync<T>(string servicePrefix, string controller);
+    }
+}
